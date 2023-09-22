3cc65d32fcece98fba25a2b193c9aa7ca3b30fa8
GustavoSilvalgs <luisgust06@gmail.com>
2023-09-22 12:36:45 -0300

    Understanding Lazy Assert Messages

--- S03_FirstStepsInJunit5WithJava/src/test/java/br/com/erudio/math/SimpleMathTest.java
+++ S03_FirstStepsInJunit5WithJava/src/test/java/br/com/erudio/math/SimpleMathTest.java
@@ -8,8 +8,16 @@ public class SimpleMathTest {
     @Test
     void testSum() {
         SimpleMath math = new SimpleMath();
-        Double actual = math.sum(6.2D, 2D);
+        double firstNumber = 6.2D;
+        double secondNumber = 2D;
+
+        Double actual = math.sum(firstNumber, secondNumber);
         double expected = 8.2D;
-        Assertions.assertEquals(expected, actual, "6.2 + 2 did not produce 8.2!");
+
+        Assertions.assertEquals(expected, actual,
+                () -> firstNumber + "+" + secondNumber +
+                    " did not produce " + expected + "!");
+        Assertions.assertNotEquals(9.2, actual);
+        Assertions.assertNotNull(actual);
     }
 }
