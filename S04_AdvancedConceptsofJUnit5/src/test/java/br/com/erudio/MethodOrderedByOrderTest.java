ec01df71d1d86f562fbc52e2f06cc50cfc97c654
GustavoSilvalgs <luisgust06@gmail.com>
2023-09-26 15:54:10 -0300

    TestMethodOrder Index-Based Ordering with annotation Order

---
+++ S04_AdvancedConceptsofJUnit5/src/test/java/br/com/erudio/MethodOrderedByOrderTest.java
@@ -0,0 +1,34 @@
+package br.com.erudio;
+
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class MethodOrderedByOrderTest {
+
+    @Test
+    @Order(4)
+    void testA(){
+        System.out.println("Running Test A");
+    }
+
+    @Test
+    @Order(3)
+    void testB(){
+        System.out.println("Running Test B");
+    }
+
+    @Test
+    @Order(2)
+    void testC(){
+        System.out.println("Running Test C");
+    }
+
+    @Test
+    @Order(1)
+    void testD(){
+        System.out.println("Running Test D");
+    }
+}
