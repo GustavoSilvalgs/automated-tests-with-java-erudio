4b605d6a4654294f7b96b2897bacc6e6da2edd64
GustavoSilvalgs <luisgust06@gmail.com>
2023-09-26 10:31:35 -0300

    Testing Performance in JUnit Tests

--- S03_FirstStepsInJunit5WithJava/src/test/java/br/com/erudio/ArraysCompareTest.java
+++ S03_FirstStepsInJunit5WithJava/src/test/java/br/com/erudio/ArraysCompareTest.java
@@ -2,8 +2,10 @@ package br.com.erudio;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
 
 import java.util.Arrays;
+import java.util.concurrent.TimeUnit;
 
 public class ArraysCompareTest {
 
@@ -14,5 +16,16 @@ public class ArraysCompareTest {
 
         Arrays.sort(numbers);
         Assertions.assertArrayEquals(numbers, expectedArray);
+    }
+
+    @Test
+//    @Timeout(1)
+    @Timeout(value = 15, unit = TimeUnit.MILLISECONDS)
+    void testSortPerformance() {
+        int[] numbers = {25,8,21,32,3};
+        for (int i = 0; i < 10000000; i++) {
+            numbers[0] = i;
+            Arrays.sort(numbers);
+        }
     }
 }
