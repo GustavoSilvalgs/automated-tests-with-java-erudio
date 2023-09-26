896abf012a3aa28eecf8f1d6653c4fdb44952f6e
GustavoSilvalgs <luisgust06@gmail.com>
2023-09-26 12:46:53 -0300

    @ParameterizedTest with CSV File

--- S04_AdvancedConceptsofJUnit5/src/test/java/br/com/erudio/math/SimpleMathTestS4.java
+++ S04_AdvancedConceptsofJUnit5/src/test/java/br/com/erudio/math/SimpleMathTestS4.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 @DisplayName("Test Math Operations in SimpleMath Class")
 public class SimpleMathTestS4 {
@@ -22,20 +22,21 @@ public class SimpleMathTestS4 {
         System.out.println("Running @AfterEach method!");
     }
 
-    @DisplayName("Test 6.2 / 2 = 3.1")
+    @DisplayName("Test double subtraction [firstNumber, secondNumber, expected]")
     @ParameterizedTest
 //    @MethodSource("testDivisionInputParameters")
 //    @MethodSource()
-    @CsvSource({
+    /**@CsvSource({
             "6.2, 2, 3.1",
             "71, 14, 5.07",
             "18.3, 3.1, 5.90"
-    })
+    })*/
     /** @CsvSource({
         "Pelé, Soccer",
         "Senna, F1",
         "Keith Moon, ''"
     })*/
+    @CsvFileSource(resources = "/testDivision.csv")
     void testDivision(double firstNumber, double secondNumber, double expected) {
 
         System.out.println("Test " + firstNumber + " / " + secondNumber + " = " + expected + "!");
