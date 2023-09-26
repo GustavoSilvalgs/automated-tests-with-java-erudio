1a3388167b3b246c62686516bc298a879a0cb3e3
GustavoSilvalgs <luisgust06@gmail.com>
2023-09-26 10:23:19 -0300

    Comparing Arrays in JUnit Tests

---
+++ S03_FirstStepsInJunit5WithJava/src/test/java/br/com/erudio/ArraysCompareTest.java
@@ -0,0 +1,18 @@
+package br.com.erudio;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+public class ArraysCompareTest {
+
+    @Test
+    void test() {
+        int[] numbers = {25,8,21,32,3};
+        int[] expectedArray = {3,8,21,25,32};
+
+        Arrays.sort(numbers);
+        Assertions.assertArrayEquals(numbers, expectedArray);
+    }
+}
