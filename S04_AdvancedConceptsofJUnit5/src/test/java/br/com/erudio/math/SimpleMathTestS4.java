0d406ca55b92e30afd1b195fa6b85c7e591c3a6a
GustavoSilvalgs <luisgust06@gmail.com>
2023-09-26 12:06:55 -0300

    @ParameterizedTest with @CsvSource

--- S04_AdvancedConceptsofJUnit5/src/test/java/br/com/erudio/math/SimpleMathTestS4.java
+++ S04_AdvancedConceptsofJUnit5/src/test/java/br/com/erudio/math/SimpleMathTestS4.java
@@ -5,10 +5,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
-
-import java.util.stream.Stream;
+import org.junit.jupiter.params.provider.CsvSource;
 
 @DisplayName("Test Math Operations in SimpleMath Class")
 public class SimpleMathTestS4 {
@@ -28,7 +25,17 @@ public class SimpleMathTestS4 {
     @DisplayName("Test 6.2 / 2 = 3.1")
     @ParameterizedTest
 //    @MethodSource("testDivisionInputParameters")
-    @MethodSource()
+//    @MethodSource()
+    @CsvSource({
+            "6.2, 2, 3.1",
+            "71, 14, 5.07",
+            "18.3, 3.1, 5.90"
+    })
+    /** @CsvSource({
+        "Pelé, Soccer",
+        "Senna, F1",
+        "Keith Moon, ''"
+    })*/
     void testDivision(double firstNumber, double secondNumber, double expected) {
 
         System.out.println("Test " + firstNumber + " / " + secondNumber + " = " + expected + "!");
@@ -40,12 +47,12 @@ public class SimpleMathTestS4 {
     }
 
 //    public static Stream<Arguments> testDivisionInputParameters() {
-    public static Stream<Arguments> testDivision() {
+    /*public static Stream<Arguments> testDivision() {
         return Stream.of(
           Arguments.of(6.2D, 2D, 3.1D),
           Arguments.of(71D, 14D, 5.07D),
           Arguments.of(18.3D, 3.1D, 5.90D)
         );
-    }
+    }*/
 
 }
