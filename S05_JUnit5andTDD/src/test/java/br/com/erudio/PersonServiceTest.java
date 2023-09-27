b7b4ba5ed257de83afbc7fdd3fdea1b7af4f23d8
GustavoSilvalgs <luisgust06@gmail.com>
2023-09-27 12:32:11 -0300

    Creating the Persons Creation Test

---
+++ S05_JUnit5andTDD/src/test/java/br/com/erudio/PersonServiceTest.java
@@ -0,0 +1,17 @@
+package br.com.erudio;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class PersonServiceTest {
+
+    @Test
+    @DisplayName("When Create a Person with Success Should Return a Person Object")
+    void testCreatePerson_WhenSuccess_ShouldReturnPersonObject() {
+        // Given / Arrange
+
+        // When / Act
+
+        // Then / Assert
+    }
+}
