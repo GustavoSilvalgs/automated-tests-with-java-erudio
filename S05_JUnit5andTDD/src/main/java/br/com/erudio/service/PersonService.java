169df20609499959704ab1e4dddce0d49ea24e62
GustavoSilvalgs <luisgust06@gmail.com>
2023-09-27 12:56:17 -0300

    Creating the PersonService

---
+++ S05_JUnit5andTDD/src/main/java/br/com/erudio/service/PersonService.java
@@ -0,0 +1,4 @@
+package br.com.erudio.service;
+
+public class PersonService implements IPersonService {
+}
--- S05_JUnit5andTDD/src/test/java/br/com/erudio/PersonServiceTest.java
+++ S05_JUnit5andTDD/src/test/java/br/com/erudio/PersonServiceTest.java
@@ -1,5 +1,7 @@
 package br.com.erudio;
 
+import br.com.erudio.service.IPersonService;
+import br.com.erudio.service.PersonService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -8,7 +10,9 @@ public class PersonServiceTest {
     @Test
     @DisplayName("When Create a Person with Success Should Return a Person Object")
     void testCreatePerson_WhenSuccess_ShouldReturnPersonObject() {
+
         // Given / Arrange
+        IPersonService service = new PersonService();
 
         // When / Act
 
