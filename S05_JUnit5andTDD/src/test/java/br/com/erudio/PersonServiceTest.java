9e01fc8613b62ba077137f627704eab3b2a3dfd6
GustavoSilvalgs <luisgust06@gmail.com>
2023-09-27 13:50:38 -0300

    Refactoring Unit Tests

--- S05_JUnit5andTDD/src/test/java/br/com/erudio/PersonServiceTest.java
+++ S05_JUnit5andTDD/src/test/java/br/com/erudio/PersonServiceTest.java
@@ -4,10 +4,25 @@ import br.com.erudio.model.Person;
 import br.com.erudio.service.IPersonService;
 import br.com.erudio.service.PersonService;
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class PersonServiceTest {
+
+    Person person;
+
+    @BeforeEach
+    void setup() {
+        person = new Person(
+                "Keith",
+                "Moon",
+                "kmoon@erudio.com.br",
+                "Wembley - UK",
+                "Male"
+        );
+    }
 
     @Test
     @DisplayName("When Create a Person with Success Should Return a Person Object")
@@ -15,14 +30,6 @@ public class PersonServiceTest {
 
         // Given / Arrange
         IPersonService service = new PersonService();
-
-        Person person = new Person(
-                "Keith",
-                "Moon",
-                "kmoon@erudio.com.br",
-                "Wembley - UK",
-                "Male"
-        );
 
         // When / Act
         Person actual = service.createPerson(person);
@@ -37,14 +44,6 @@ public class PersonServiceTest {
 
         // Given / Arrange
         IPersonService service = new PersonService();
-
-        Person person = new Person(
-                "Keith",
-                "Moon",
-                "kmoon@erudio.com.br",
-                "Wembley - UK",
-                "Male"
-        );
 
         // When / Act
         Person actual = service.createPerson(person);
