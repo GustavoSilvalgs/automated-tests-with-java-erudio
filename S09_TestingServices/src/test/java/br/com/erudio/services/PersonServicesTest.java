094eebc91618bb5388cc7b005afab4d780986568
GustavoSilvalgs <luisgust06@gmail.com>
2023-10-05 12:15:28 -0300

    Testing the save Method of PersonService

---
+++ S09_TestingServices/src/test/java/br/com/erudio/services/PersonServicesTest.java
@@ -0,0 +1,56 @@
+package br.com.erudio.services;
+
+import br.com.erudio.model.Person;
+import br.com.erudio.repositories.PersonRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.BDDMockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class PersonServicesTest {
+
+	@Mock
+	private PersonRepository repository;
+
+	@InjectMocks
+	private PersonServices service;
+
+	private Person person0;
+
+	@BeforeEach
+	public void setup() {
+		// Given / Arrange
+		person0 = new Person(
+				"Gustavo",
+				"Silva",
+				"gustavo@gustavo.com",
+				"Mogi das Cruzes - São Paulo - Brasil",
+				"male");
+	}
+
+	@DisplayName("JUnit test for Given Person Object When Save Person then Return Person Object")
+	@Test
+	void testGivenPersonObject_WhenSavePerson_thenReturnPersonObject() {
+	    // Given / Arrange
+		given(repository.findByEmail(anyString())).willReturn(Optional.empty());
+		given(repository.save(person0)).willReturn(person0);
+
+	    // When / Act
+		Person savedPerson = service.create(person0);
+
+	    // Then / Assert
+		assertNotNull(savedPerson);
+		assertEquals("Gustavo", savedPerson.getFirstName());
+
+	}
+}
