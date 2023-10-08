65d54bb0323a065720cea55e01e07029a9ff410c
GustavoSilvalgs <luisgust06@gmail.com>
2023-10-08 20:56:18 -0300

    Preparing Test Infrastructure for PersonController Endpoints

---
+++ S11_IntegrationTests/src/test/java/br/com/erudio/integrationtests/controller/PersonControllerIntegrationTest.java
@@ -0,0 +1,46 @@
+package br.com.erudio.integrationtests.controller;
+
+import br.com.erudio.configs.TestConfigs;
+import br.com.erudio.integrationtests.testcontainers.AbstractIntegrationTest;
+import br.com.erudio.model.Person;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.filter.log.LogDetail;
+import io.restassured.filter.log.RequestLoggingFilter;
+import io.restassured.filter.log.ResponseLoggingFilter;
+import io.restassured.specification.RequestSpecification;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+class PersonControllerIntegrationTest extends AbstractIntegrationTest {
+
+	private static RequestSpecification specification;
+	private static ObjectMapper objectMapper;
+	private static Person person;
+
+	@BeforeAll
+	public void setup() {
+		// Given / Arrange
+		objectMapper = new ObjectMapper();
+		objectMapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+
+		specification = new RequestSpecBuilder()
+				.setBasePath("")
+				.setPort(TestConfigs.SERVER_PORT)
+				.addFilter(new RequestLoggingFilter(LogDetail.ALL))
+				.addFilter(new ResponseLoggingFilter(LogDetail.ALL))
+				.build();
+
+		person = new Person(
+				"Gustavo",
+				"Silva",
+				"gustavo@gustavo.com",
+				"Mogi das Cruzes - São Paulo - Brasil",
+				"male");
+	}
+}
